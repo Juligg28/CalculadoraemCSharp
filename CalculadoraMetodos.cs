40e328d2059fe981b3ba4353d930c885495fe4b6
Juli Goes <goesjuliene28@gmail.com>
2023-11-13 10:39:43 -0300

    Add files via upload

---
+++ CalculadoraMetodos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculadora
+{
+       
+    
+    public class CalculatoraMetodos
+    {
+        public int Adicao(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divisao(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Nao pode dividir por zero");
+
+            return a / b;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculatoraMetodos calculatora = new CalculatoraMetodos();
+
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    ExecuteOperacao(calculatora.Adicao, "adição");
+                    break;
+                case 2:
+                    ExecuteOperacao(calculatora.Subtracao, "subtração");
+                    break;
+                case 3:
+                    ExecuteOperacao(calculatora.Multiplicacao, "multiplicação");
+                    break;
+                case 4:
+                    ExecuteOperacao(calculatora.Divisao, "divisão");
+                    break;
+                default:
+                    Console.WriteLine("Escolha inválida.");
+                    break;
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadLine();
+        }
+                
+        private static void ExecuteOperacao(Func<int, int, int> operacao, string nomeOperacao)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = operacao(n1, n2);
+
+            Console.WriteLine($"Resultado da {nomeOperacao}: {resultado}");
+        }
+    }
+}
